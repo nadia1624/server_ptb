0d17e947228f3482aa11129b25fc05ae415b5f18
nadia1624 <nadyadearihanifah@gmail.com>
2024-12-17 17:52:00 +0700

    konfigurasi production

--- seeders/20241121054243-demo-kegiatan.js
+++ seeders/20241121054243-demo-kegiatan.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         nama_kegiatan: 'Workshop UI/UX',
         deskripsi: 'Workshop tentang UI/UX Design untuk mahasiswa.',
-        jam_kegiatan: '10:00 AM',
+        jam_kegiatan: '10:00',
         tanggal_kegiatan: '2024-11-21',
         status: 1,  // Kegiatan terbuka untuk absensi
         createdAt: new Date(),
@@ -15,7 +15,7 @@ module.exports = {
       {
         nama_kegiatan: 'Seminar Teknologi',
         deskripsi: 'Seminar tentang perkembangan teknologi terbaru.',
-        jam_kegiatan: '01:00 PM',
+        jam_kegiatan: '13:00',
         tanggal_kegiatan: '2024-11-20',
         status: 0,  // Kegiatan sudah ditutup, tidak bisa absensi
         createdAt: new Date(),
@@ -24,7 +24,7 @@ module.exports = {
       {
         nama_kegiatan: 'Pelatihan Pemrograman Python',
         deskripsi: 'Pelatihan dasar pemrograman Python untuk pemula.',
-        jam_kegiatan: '09:00 AM',
+        jam_kegiatan: '09:00',
         tanggal_kegiatan: '2024-11-22',
         status: 1,  // Kegiatan terbuka untuk absensi
         createdAt: new Date(),
@@ -33,7 +33,7 @@ module.exports = {
       {
         nama_kegiatan: 'Kursus Data Science',
         deskripsi: 'Kursus mengenai data science dan analisis data.',
-        jam_kegiatan: '02:00 PM',
+        jam_kegiatan: '14:00',
         tanggal_kegiatan: '2024-11-23',
         status: 0,  // Kegiatan sudah ditutup, tidak bisa absensi
         createdAt: new Date(),
